e05764cf62f0701903c002a8e9e0c54ed995e96c
Chunpeng Huo <huocp@me.com>
2020-02-17 16:11:38 +1100

    test: sometimes powershell did not alias cat to Get-Content

--- test/e2e-chrome.spec.js
+++ test/e2e-chrome.spec.js
@@ -1,79 +1,73 @@
 const test = require('tape');
 const {exec} = require('child_process');
+const cat = require('./_cat');
 
-if (process.env['TRAVIS_OS_NAME'] !== 'windows') {
-  test('browser-do:chrome detects passed tape tests', t => {
-    exec('npx browserify test/samples/_tape-good.js | node bin/browser-do.js --tap -b chrome-headless', error => {
-      t.notOk(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome detects failed tape tests', t => {
-    exec('npx browserify test/samples/_tape-bad.js | node bin/browser-do.js --tap -b chrome-headless', error => {
-      t.ok(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome detects passed jasmine tests', t => {
-    exec('npx browserify test/samples/_jasmine-good.js | node bin/browser-do.js --jasmine -b chrome-headless', error => {
-      t.notOk(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome detects failed jasmine tests', t => {
-    exec('npx browserify test/samples/_jasmine-bad.js | node bin/browser-do.js --jasmine -b chrome-headless', error => {
-      t.ok(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome detects passed mocha tests', t => {
-    exec('npx browserify test/samples/_mocha-good.js | node bin/browser-do.js --mocha -b chrome-headless', error => {
-      t.notOk(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome detects failed mocha tests', t => {
-    exec('npx browserify test/samples/_mocha-bad.js | node bin/browser-do.js --mocha -b chrome-headless', error => {
-      t.ok(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome supports static assets and html input', t => {
-    exec('cat test/_jasmine-good.html | node bin/browser-do.js --jasmine --static test/samples -b chrome-headless', error => {
-      t.notOk(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome supports static assets and html input, with failed tests', t => {
-    exec('cat test/_jasmine-bad.html | node bin/browser-do.js --jasmine --static test/samples -b chrome-headless', error => {
-      t.ok(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome supports mock and html input', t => {
-    exec('cat test/_mock-jasmine-good.html | node bin/browser-do.js --jasmine --mock test/_mock.js -b chrome-headless', error => {
-      t.notOk(error);
-      t.end();
-    });
-  });
-
-  test('browser-do:chrome supports mock and html input, with failed tests', t => {
-    exec('cat test/_mock-jasmine-bad.html | node bin/browser-do.js --jasmine --mock test/_mock.js -b chrome-headless', error => {
-      t.ok(error);
-      t.end();
-    });
-  });
-} else {
-  test('bypass chrome on travis-ci windows box', t => {
-    t.pass('bypass chrome on travis-ci windows box');
+test('browser-do:chrome detects passed tape tests', t => {
+  exec('npx browserify test/samples/_tape-good.js | node bin/browser-do.js --tap -b chrome-headless', error => {
+    t.notOk(error);
     t.end();
   });
-}
+});
+
+test('browser-do:chrome detects failed tape tests', t => {
+  exec('npx browserify test/samples/_tape-bad.js | node bin/browser-do.js --tap -b chrome-headless', error => {
+    t.ok(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome detects passed jasmine tests', t => {
+  exec('npx browserify test/samples/_jasmine-good.js | node bin/browser-do.js --jasmine -b chrome-headless', error => {
+    t.notOk(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome detects failed jasmine tests', t => {
+  exec('npx browserify test/samples/_jasmine-bad.js | node bin/browser-do.js --jasmine -b chrome-headless', error => {
+    t.ok(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome detects passed mocha tests', t => {
+  exec('npx browserify test/samples/_mocha-good.js | node bin/browser-do.js --mocha -b chrome-headless', error => {
+    t.notOk(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome detects failed mocha tests', t => {
+  exec('npx browserify test/samples/_mocha-bad.js | node bin/browser-do.js --mocha -b chrome-headless', error => {
+    t.ok(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome supports static assets and html input', t => {
+  exec(cat + ' test/_jasmine-good.html | node bin/browser-do.js --jasmine --static test/samples -b chrome-headless', error => {
+    t.notOk(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome supports static assets and html input, with failed tests', t => {
+  exec(cat + ' test/_jasmine-bad.html | node bin/browser-do.js --jasmine --static test/samples -b chrome-headless', error => {
+    t.ok(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome supports mock and html input', t => {
+  exec(cat + ' test/_mock-jasmine-good.html | node bin/browser-do.js --jasmine --mock test/_mock.js -b chrome-headless', error => {
+    t.notOk(error);
+    t.end();
+  });
+});
+
+test('browser-do:chrome supports mock and html input, with failed tests', t => {
+  exec(cat + ' test/_mock-jasmine-bad.html | node bin/browser-do.js --jasmine --mock test/_mock.js -b chrome-headless', error => {
+    t.ok(error);
+    t.end();
+  });
+});
--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -1,6 +1,7 @@
 const test = require('tape');
 const {exec, execSync} = require('child_process');
 const {version} = require('../package.json');
+const cat = require('./_cat');
 
 test('browser-do:electron prints out version number', t => {
   t.equal(execSync('node bin/browser-do.js --version').toString().trim(), version);
@@ -50,28 +51,28 @@ test('browser-do:electron detects failed mocha tests', t => {
 });
 
 test('browser-do:electron supports static assets and html input', t => {
-  exec('cat test/_jasmine-good.html | node bin/browser-do.js --jasmine --static test/samples', error => {
+  exec(cat + ' test/_jasmine-good.html | node bin/browser-do.js --jasmine --static test/samples', error => {
     t.notOk(error);
     t.end();
   });
 });
 
 test('browser-do:electron supports static assets and html input, with failed tests', t => {
-  exec('cat test/_jasmine-bad.html | node bin/browser-do.js --jasmine --static test/samples', error => {
+  exec(cat + ' test/_jasmine-bad.html | node bin/browser-do.js --jasmine --static test/samples', error => {
     t.ok(error);
     t.end();
   });
 });
 
 test('browser-do:electron supports mock and html input', t => {
-  exec('cat test/_mock-jasmine-good.html | node bin/browser-do.js --jasmine --mock test/_mock.js', error => {
+  exec(cat + ' test/_mock-jasmine-good.html | node bin/browser-do.js --jasmine --mock test/_mock.js', error => {
     t.notOk(error);
     t.end();
   });
 });
 
 test('browser-do:electron supports mock and html input, with failed tests', t => {
-  exec('cat test/_mock-jasmine-bad.html | node bin/browser-do.js --jasmine --mock test/_mock.js', error => {
+  exec(cat + ' test/_mock-jasmine-bad.html | node bin/browser-do.js --jasmine --mock test/_mock.js', error => {
     t.ok(error);
     t.end();
   });
