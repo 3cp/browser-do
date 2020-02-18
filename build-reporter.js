2a8fc38f8d5dc3b872c210729831810db096a1ce
Chunpeng Huo <huocp@me.com>
2020-02-18 11:35:21 +1100

    fix: fix reporter build

--- build-reporter.js
+++ build-reporter.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 const browserify = require('browserify');
 const es6ify = require("es6ify");
-browserify('./reporter.js')
+
+try {
+  fs.mkdirSync('dist');
+} catch (e) {
+  // ignore
+}
+
+browserify('reporter.js')
   .add(es6ify.runtime)
   .transform(es6ify)
   .bundle()
-  .pipe(fs.createWriteStream('./dist/reporter.js'));
+  .pipe(fs.createWriteStream('dist/reporter.js'));
