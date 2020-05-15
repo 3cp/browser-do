77678f361b4d3565c575b2ceba22f0187201c844
Chunpeng Huo <huocp@me.com>
2020-05-16 00:30:51 +1000

    chore: remove duplexer

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 /* eslint-disable no-console */
 const run = require('./lib/browser-run');
-const {Writable, Transform} = require('stream');
-const duplex = require('duplexer');
+const {Transform} = require('stream');
 const tapParse = require('./lib/tap-parse');
 
 module.exports = function(opts = {}) {
   const tap = opts.tap || opts.tape || opts.jasmine || opts.mocha;
 
   const chunks = [];
-  const readInput = new Writable({
-    write(chunk, enc, cb) {
+  const dpl = new Transform({
+    transform(chunk, enc, cb) {
       chunks.push(chunk);
       cb();
-    }
-  });
-
-  const holdOutput = new Transform({
-    transform(chunk, enc, cb) {
-      cb(null, chunk);
-    }
-  });
-  const dpl = duplex(readInput, holdOutput);
-
-  dpl.failed = false;
-  let browserDo;
-  dpl.stop = () => browserDo && browserDo.stop();
-
-  readInput.on('finish', () => {
-    const data = Buffer.concat(chunks).toString();
-    browserDo = run(opts, data, holdOutput);
-
-    if (tap) {
-      tapParse(holdOutput, (err, passed) => {
-        dpl.failed = !passed;
-
-        if (err) {
-          console.error(err.message);
-        }
-
-        if (opts.onCoverage) {
-          browserDo.askCoverage();
-        }
-
-        if (!opts.keepOpen) {
-          setTimeout(() => {
-            if (opts.onCoverage) {
-              browserDo.checkCoverage(opts.onCoverage);
-            }
-            dpl.stop();
-            dpl.emit('exit', dpl.failed ? 1 : 0);
-          }, 1000);
+    },
+    flush(cb) {
+      const self = this;
+      let failed = false;
+      let browserDo;
+      self.stop = () => browserDo && browserDo.stop();
+      const holdOutput = new Transform({
+        transform(chunk, enc, _cb) {
+          _cb(null, chunk);
+          self.push(chunk);
+        },
+        flush(_cb) {
+          if (!tap) cb();
+          _cb();
         }
       });
+
+      const data = Buffer.concat(chunks).toString();
+      browserDo = run(opts, data, holdOutput);
+
+      if (tap) {
+        tapParse(holdOutput, (err, passed) => {
+          failed = !passed;
+
+          if (err) {
+            console.error(err.message);
+          }
+
+          if (opts.onCoverage) {
+            browserDo.askCoverage();
+          }
+
+          if (!opts.keepOpen) {
+            setTimeout(() => {
+              if (opts.onCoverage) {
+                browserDo.checkCoverage(opts.onCoverage);
+              }
+              self.stop();
+              cb();
+              self.emit('exit', failed ? 1 : 0);
+            }, 1000);
+          }
+        });
+      }
     }
   });
 
